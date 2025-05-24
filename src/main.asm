1f2cfeeb0bc7fa93c7226fc01bba91ce6b495afd
toasties-with-toast <imatoaster65@gmail.com>
2025-05-24 15:52:18 +1000

    Setting up the data segments and stack (main.asm)

--- src/main.asm
+++ src/main.asm
@@ -2,7 +2,17 @@ org 0x7c00
 bits 16
 
 main: 
+  ; setup data segments
+  mov ax, 0   ; can't write to es/ds directly, 
+  mov ds, ax  ; so you must mov from another register
+  mov es, ax
+
+  ;setup stack
+  mov ss, ax
+  mov sp, 0x7c00 ; the stack goes downwards, and this is setting it to go downwards from the start of our program, to ensure it doesn't overwrite any code
+
   hlt
+
 .halt
   jmp .halt
 
