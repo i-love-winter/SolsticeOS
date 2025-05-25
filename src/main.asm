7ddc24557ec9f237dae9b459ee731cf2e7452bd1
toasties-with-toast <imatoaster65@gmail.com>
2025-05-25 12:41:17 +1000

    Implement simple-ish string printing (main.asm)

--- src/main.asm
+++ src/main.asm
@@ -1,6 +1,30 @@
 org 0x7c00
 bits 16
 
+%define ENDL 0x0D, 0x0A
+
+start:
+  jmp main ; ensuring main is the start of the program
+
+; implementing a simple-ish print function
+
+putstr: ; save registers that are about to be edited
+  push si
+  push ax
+.printloop:
+  lodsb ; loading next character in al
+  or al, al ; check if next charcater is null
+  jz .done
+  
+  mov ah, 0x0e ; call bios interrupt
+  int 0x10
+
+  jmp .printloop
+.done:
+  pop ax
+  pop si
+  ret
+
 main: 
   ; setup data segments
   mov ax, 0   ; can't write to es/ds directly, 
@@ -11,10 +35,17 @@ main:
   mov ss, ax
   mov sp, 0x7c00 ; the stack goes downwards, and this is setting it to go downwards from the start of our program, to ensure it doesn't overwrite any code
 
-  hlt
+  ; print message 
+  mov si, printtext
+  call putstr
 
-.halt
+  hlt
+.halt:
   jmp .halt
+
+printtext: db '########################', 10, 13, ; ten and 13 are ASCII's way of doing newlines
+           db '# This OS Needs A Name #', 10, 13,
+           db '########################', ENDL,  0
 
 times 510-($-$$) db 0
 dw 0AA55h
