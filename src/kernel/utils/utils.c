f91a9c8b43ba1d22bf5aa91f30be531bd4d7b6b2
aether123 <imatoaster65@gmail.com>
2025-06-22 08:58:11 +1000

    Update utils.c

--- src/kernel/utils/utils.c
+++ src/kernel/utils/utils.c
@@ -7,3 +7,19 @@ void memset(void *dest, char val, uint32_t count) {
     *temp++ = val;
   }
 }
+
+void vga_print(const char *str) {
+    volatile unsigned short *vidmem = (unsigned short *)0xB8000; // VGA text buffer
+    int row = 0, col = 0;
+
+    while (*str) {
+        if (*str == '\n') {
+            row++; // Move down a row
+            col = 0; // Reset column position
+            vidmem = (volatile unsigned short *)0xB8000 + row * 80; // Move to start of next row
+        } else {
+            vidmem[col++] = (*str & 0xFF) | (0x07 << 8); // White text on black
+        }
+        str++;
+    }
+}
